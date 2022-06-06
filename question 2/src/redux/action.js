39d37e95cf90f8fa366655aa365703b8a55aa5be
Tushar-chauhan198 <tc21085@gmail.com>
2022-06-06 16:17:45 +0530

    done

--- question 2/src/redux/action.js
+++ question 2/src/redux/action.js
@@ -1,9 +1,20 @@
 export const actionTypes = {
   // key value pair
+  ADD_USER_REQUEST:" ADD_USER_REQUEST",
+ADD_USER_SUCCESS:"ADD_USER_SUCCESS",
+ADD_USER_FAILURE:"ADD_USER_FAILURE"
 };
+
 // do not change names
-export const addUserRequest = () => {};
+export const addUserRequest = () => ({
+  type:actionTypes.ADD_USER_REQUEST
+});
 // do not change names, and arguments
-export const addUserSuccess = (data) => {};
+export const addUserSuccess = ({name,id,title}) => ({
+  type:actionTypes.ADD_USER_SUCCESS,
+  payload:data
+});
 // do not change names
-export const addUserFailure = () => {};
+export const addUserFailure = () => ({
+  type:actionTypes.ADD_USER_FAILURE
+});
--- question 2/src/redux/reducer.js
+++ question 2/src/redux/reducer.js
@@ -1,9 +1,38 @@
 import { actionTypes } from "./action";
 
-const initState = {};
+const initState = {
+    isLoading:false,
+    isError:false,
+    data:[]
+};
 
 function reducer(state = initState, action) {
     // write code here
+    switch(action.type){
+        case actionTypes.ADD_USER_REQUEST:{
+            return {
+                ...state,
+                isLoading:true,
+                isError:false
+            }
+        }
+        case actionTypes.ADD_USER_SUCCESS:{
+            return {
+                ...state,
+                isLoading:false,
+                data:action.payload
+            }
+        }
+        case actionTypes.ADD_USER_FAILURE:{
+            return {
+                ...state,
+                isLoading:false,
+                isError:true
+            }
+        }
+        default:
+            return state
+    }
 }
 
 export default reducer;
