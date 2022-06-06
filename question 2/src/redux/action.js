49aa29b25b6ff926a4c06465d6d693d931b535af
Tushar-chauhan198 <tc21085@gmail.com>
2022-06-06 14:57:27 +0530

    add
    add

---
+++ question 2/src/redux/action.js
@@ -0,0 +1,9 @@
+export const actionTypes = {
+  // key value pair
+};
+// do not change names
+export const addUserRequest = () => {};
+// do not change names, and arguments
+export const addUserSuccess = (data) => {};
+// do not change names
+export const addUserFailure = () => {};
---
+++ question 2/src/redux/reducer.js
@@ -0,0 +1,9 @@
+import { actionTypes } from "./action";
+
+const initState = {};
+
+function reducer(state = initState, action) {
+    // write code here
+}
+
+export default reducer;
